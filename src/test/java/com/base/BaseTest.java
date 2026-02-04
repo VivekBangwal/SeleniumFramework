eadf93be1ae0501e8d8ae67ecfbff6cb9fc09966
Vivek Bangwal <Vivek.Bangwal@coforge.com>
2026-02-04 14:11:00 +0530

    first commit

---
+++ src/test/java/com/base/BaseTest.java
@@ -0,0 +1,29 @@
+package com.base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+
+import com.driver.DriverFactory;
+import com.utils.PropertyUtils;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    // Launch browser and open application
+    @BeforeMethod
+    public void setup() throws InterruptedException {
+        String browser = PropertyUtils.getProperty("browser");
+        driver = DriverFactory.initDriver(browser);
+        driver.get(PropertyUtils.getProperty("baseUrl"));
+        Thread.sleep(3000);
+    }
+
+    // Close browser
+    @AfterMethod
+    public void tearDown() {
+    	if(driver != null) {
+    		driver.quit();
+    	}
+    }
+}
